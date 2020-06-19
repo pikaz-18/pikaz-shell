253ade72f0d988c35dd8115e0827ee2141661ed9
zouzheng <qq2363994579@outlook.com>
2020-06-19 13:54:58 +0800

    修改demo

--- src/example.js
+++ src/example.js
@@ -2,9 +2,9 @@
  * @Author: zouzheng
  * @Date: 2020-06-19 09:32:45
  * @LastEditors: zouzheng
- * @LastEditTime: 2020-06-19 11:36:27
+ * @LastEditTime: 2020-06-19 13:54:28
  * @Description: 这是demo组件（页面）
  */
 import shell from './index'
 
-shell([{ cmd: ["tree>tree.txt", "tree>tree1.txt"] }, { cmd: ["mkdir pikaz-shell"], path: "D:\\" }])
+shell([{ cmd: ["tree>tree1.txt", "tree>tree2.txt"] }, { cmd: ["mkdir pikaz-shell"], path: "D:\\" }])
