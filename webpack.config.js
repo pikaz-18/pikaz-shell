af5964e3cedb47a9c126096963c78acfdfb44034
zouzheng <qq2363994579@outlook.com>
2020-06-19 11:28:34 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@
  * @Author: zouzheng
  * @Date: 2020-05-25 11:19:28
  * @LastEditors: zouzheng
- * @LastEditTime: 2020-06-19 09:53:03
- * @Description: 这是XXX组件（页面）
+ * @LastEditTime: 2020-06-19 11:28:23
+ * @Description: 这是webpack配置组件（页面）
  */
 const path = require('path');
 
